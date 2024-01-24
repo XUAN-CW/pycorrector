ea8f4f5fc3190fda78fffd15a185d6a443296244
xuanchengwei <xuanchengwei@coros.com>
2024-01-24 13:37:02 +0800

    cd

--- fiction_correction/md.py
+++ fiction_correction/md.py
@@ -71,5 +71,6 @@ if __name__ == '__main__':
             # print(result_json)
             # print(r)
             json_string = json.dumps(r, ensure_ascii=False)
-            print(json_string.replace(r['source'],error_sentence))
+            print(json_string.replace(r['source'],error_sentence) + "\n")
+
 
